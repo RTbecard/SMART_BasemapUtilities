d6c3524664d01356c2ba0f83b9b61438a15875b9
RTbecard <jamesadamcampbell@gmail.com>
2018-11-27 22:41:11 +0100

    Added support for google API key using forked dismo package

--- server.R
+++ server.R
@@ -2,13 +2,18 @@
 require(rgdal)
 require(raster)
 require(dismo)
+require(ggmap)
 
 shinyServer(function(input, output) {
   ext <- reactive({
     extent(as.numeric(c(input$xmin,input$xmax,input$ymin,input$ymax)))
   })
-  
-  mapType <- reactive({
+
+  ## Register google API key
+  register_google(key = "insertGoogleKeyHere")
+  message("Google API key accepted: " ,has_google_key())  
+
+    mapType <- reactive({
     # Fetch the appropriate data object, depending on the value
     # of input$dataset.
     return(switch(input$type,
@@ -34,26 +39,27 @@ shinyServer(function(input, output) {
     # This function should write data to a file given to it by
     # the argument 'file'.
     content = function(file){
-      browser()
+      #browser()
       if(mapType()==1){ 
         gmap(ext(),rgb = T,
                     scale = 2,
                     filename = 'Satellite.gif',
-                    type = 'satellite')
+                    type = 'satellite', googleAPIkey = google_key())
         zip(file,c('Satellite.gfw','Satellite.prj','Satellite.gif'))
       }else{
         gmap(ext(),rgb = T,
                     scale = 2,
                     filename = 'Topographic.gif',
-                    type = 'terrain')
+                    type = 'terrain', googleAPIkey = google_key())
         zip(file,c('Topographic.gfw','Topographic.prj','Topographic.gif'))}
     }
     )
   
   output$plotMap <- renderPlot({
     map <- switch(mapType(), 
-           plotRGB(gmap(ext(),rgb = T,scale = 2,type = 'satellite')),
-           plotRGB(gmap(ext(),rgb = T,scale = 2,type = 'terrain')))
+           plotRGB(gmap(ext(),rgb = T,scale = 2,type = 'satellite', googleAPIkey = google_key())),
+           plotRGB(gmap(ext(),rgb = T,scale = 2,type = 'terrain', googleAPIkey = google_key())))
     }
   )
 })
+
